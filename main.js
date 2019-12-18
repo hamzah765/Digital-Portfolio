3e50076a46ab3ab798b6b801bedb8d6392dcc4cd
hamzah765 <hamzahkanth@gmail.com>
2019-12-18 16:04:20 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// const proj = document.getElementById('.projects')
+// const aboutme = document.getElementById('.aboutme')
+// const journey = document.getElementById('.journey')
+// const future = document.getElementById('.future')
